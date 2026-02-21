bcc2af37ba10f1be901e0b131bc3e6edaed31f2f
Tee_me2 <114180127+Timmydavid123@users.noreply.github.com>
2026-02-21 04:54:03 +0000

    Fixed

--- clauseguard/settings.py
+++ clauseguard/settings.py
@@ -7,7 +7,11 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dev-key-change
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = [
+    "clauseguard-ai-st8u.onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 INSTALLED_APPS = [
     'django.contrib.staticfiles',
